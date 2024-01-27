6ef40d705dbf1c7698d835bd8a1a5719fd66cc87
i-Rony-op <raunaksharma6205@gmail.com>
2024-01-27 12:19:43 +0530

    animations

--- script.js
+++ script.js
@@ -36,11 +36,13 @@ function firstpgAnime() {
 firstpgAnime();
 
 document.querySelectorAll(".elem").forEach(function (elem) {
-  elem.addEventListener("mousemove", function (details) {
-    console.log(elem.getBoundingClientRect());
+  elem.addEventListener("mousemove", function (dets) {
+    var diff = dets.clientY - elem.getBoundingClientRect().top;
     gsap.to(elem.querySelector("img"), {
       opacity: 1,
       ease: Power1,
+      top: diff,
+      left: dets.clientX,
     });
   });
 });
